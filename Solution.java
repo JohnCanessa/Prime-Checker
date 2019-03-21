73ccaa6d656e38e0c7b387e4958beac103c50308
John Canessa <8019504+JohnCanessa@users.noreply.github.com>
2019-03-21 08:24:20 -0500

    Add files via upload
    Modification to introduce "in" issue and address it.

--- Solution.java
+++ Solution.java
@@ -3,7 +3,63 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+import java.io.BufferedReader;
 import java.lang.reflect.*;
+
+
+// **** address the input issue (part of the challenge) ****
+import static java.lang.System.in;
+
+
+/*
+ * Requested class name for the challenge.
+ */
+class Prime {
+	
+	/*
+	 * Determine if this number is prime or not.
+	 */
+	private boolean isPrime(int n) {
+		
+		// **** number <= 1 are not prime ****
+		if (n <= 1)
+			return false;
+		
+		// **** 2 and 3 are prime ****
+		if (n <= 3)
+			return true;
+		
+		// **** skip multiples of 2 and 3 ****
+		if ((n % 2 == 0) ||				// even
+			(n % 3 == 0))				// start at 3
+			return false;
+		
+		// **** start with 3 and increment by 2 ****
+		int sr = (int)Math.sqrt(n);
+		for (int i = 3; i <= sr; i += 2) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		
+		// **** should be a prime ****
+		return true;
+	}
+	
+	
+	/*
+	 * Helper function to loop through the numbers and display primes
+	 */
+	void checkPrime(int... numbers) {
+		
+		// **** ****
+		for (int n : numbers) {
+			if (isPrime(n))
+				System.out.print(n + " ");
+		}
+		System.out.println();
+	}
+}
 
 
 /*
@@ -14,61 +70,12 @@ public class Solution {
 	/*
 	 * 
 	 */
-	static class Prime {
-		
-		/*
-		 * Determine if this number is prime or not.
-		 */
-		private boolean isPrime(int n) {
-			
-			// **** number <= 1 are not prime ****
-			if (n <= 1)
-				return false;
-			
-			// **** 2 and 3 are prime ****
-			if (n <= 3)
-				return true;
-			
-			// **** skip multiples of 2 and 3 ****
-			if ((n % 2 == 0) ||				// even
-				(n % 3 == 0))				// start at 3
-				return false;
-			
-			// **** start with 3 and increment by 2 ****
-			int sr = (int)Math.sqrt(n);
-			for (int i = 3; i <= sr; i += 2) {
-				if (n % i == 0) {
-					return false;
-				}
-			}
-			
-			// **** should be a prime ****
-			return true;
-		}
-		
-		
-		/*
-		 * Helper function to loop through the numbers and display primes
-		 */
-		void checkPrime(int... numbers) {
-			
-			// **** ****
-			for (int n : numbers) {
-				if (isPrime(n))
-					System.out.print(n + " ");
-			}
-			System.out.println();
-		}
-	}
-	
-	
-	/*
-	 * 
-	 */
 	public static void main(String[] args) {
 		try {
 			
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			// **** address this bug (introduced by HackerRank) ****
+			BufferedReader br=new BufferedReader(new InputStreamReader(in));
+//			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 			
 			int n1=Integer.parseInt(br.readLine());
 			int n2=Integer.parseInt(br.readLine());
